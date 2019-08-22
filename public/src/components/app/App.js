8f59b4e082e3e6b62f426a8a17dde0cf5b3d94c9
Dave Trost <davetrost@gmail.com>
2019-08-22 15:26:51 -0700

    reorganize css files

---
+++ public/src/components/app/App.js
@@ -0,0 +1,13 @@
+import Component from '../Component.js';
+
+export class App extends Component {
+
+    onRender(dom) {
+
+    }
+
+    renderHTML() {
+        return /*html*/ `
+        `;
+    }
+}
