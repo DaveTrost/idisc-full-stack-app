7e06e949c0dc1fe678ebc15066ae640316b193fc
Dave Trost <davetrost@gmail.com>
2019-08-26 12:00:00 -0700

    add scorecard (render, style, api & server)

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(morgan('dev'));
 app.use(cors());
+app.use(express.json());
 app.use(express.static('public'));
+
 
 app.get('/api/scorecards', (req, res) => {
     client.query(`
@@ -27,6 +29,20 @@ app.get('/api/scorecards', (req, res) => {
         FROM scorecards;
     `)
         .then(databaseResponse => res.status(200).json(databaseResponse.rows))
+        .catch(err => res.status(500).json({ error: err.message || err }))
+    ;
+});
+
+app.post('/api/scorecards', (req, res) => {
+    client.query(`
+        INSERT
+        INTO scorecards(url, course_name, date, score, score_to_par, is_rated_round)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING *;
+    `,
+    [req.body.url, req.body.course, req.body.date, req.body.score, req.body.toPar, req.body.isRated]
+    )
+        .then(databaseResponse => res.status(200).json(databaseResponse.rows[0]))
         .catch(err => res.status(500).json({ error: err.message || err }))
     ;
 });
